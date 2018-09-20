b2fe38dd0d472e7fafc58a7c870e0bc0b960b775
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-09-20 10:14:11 +0200

    init app

---
+++ sql.sql
@@ -0,0 +1,13 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  mahdy
+ * Created: Sep 20, 2018
+ */
+
+CREATE SCHEMA `admin-dashboard` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ;
+
+
