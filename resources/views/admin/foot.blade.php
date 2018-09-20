fe4d49f3de063192aab1d9dcbdd5e21dea5a43d6
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-09-20 12:09:19 +0200

    separate layout_blank_page.html to multipages in blade

---
+++ resources/views/admin/foot.blade.php
@@ -0,0 +1,28 @@
+<!-- BEGIN JAVASCRIPTS(Load javascripts at bottom, this will reduce page load time) -->
+<!-- BEGIN CORE PLUGINS -->
+<!--[if lt IE 9]>
+<script src="{{ url("admin/assets/global/plugins/respond.min.js") }}"></script>
+<script src="{{ url("admin/assets/global/plugins/excanvas.min.js") }}"></script> 
+<![endif]-->
+<script src="{{ url("admin/assets/global/plugins/jquery.min.js") }}" type="text/javascript"></script>
+<script src="{{ url("admin/assets/global/plugins/jquery-migrate.min.js") }}" type="text/javascript"></script>
+<!-- IMPORTANT! Load jquery-ui.min.js before bootstrap.min.js to fix bootstrap tooltip conflict with jquery ui tooltip -->
+<script src="{{ url("admin/assets/global/plugins/jquery-ui/jquery-ui.min.js") }}" type="text/javascript"></script>
+<script src="{{ url("admin/assets/global/plugins/bootstrap/js/bootstrap.min.js") }}" type="text/javascript"></script>
+<script src="{{ url("admin/assets/global/plugins/bootstrap-hover-dropdown/bootstrap-hover-dropdown.min.js") }}" type="text/javascript"></script>
+<script src="{{ url("admin/assets/global/plugins/jquery-slimscroll/jquery.slimscroll.min.js") }}" type="text/javascript"></script>
+<script src="{{ url("admin/assets/global/plugins/jquery.blockui.min.js") }}" type="text/javascript"></script>
+<script src="{{ url("admin/assets/global/plugins/jquery.cokie.min.js") }}" type="text/javascript"></script>
+<script src="{{ url("admin/assets/global/plugins/uniform/jquery.uniform.min.js") }}" type="text/javascript"></script>
+<script src="{{ url("admin/assets/global/plugins/bootstrap-switch/js/bootstrap-switch.min.js") }}" type="text/javascript"></script>
+<!-- END CORE PLUGINS -->
+<script src="{{ url("admin/assets/global/scripts/metronic.js") }}" type="text/javascript"></script>
+<script src="{{ url("admin/assets/admin/layout4/scripts/layout.js") }}" type="text/javascript"></script>
+<script src="{{ url("admin/assets/admin/layout4/scripts/demo.js") }}" type="text/javascript"></script>
+<script>
+jQuery(document).ready(function () {
+    Metronic.init(); // init metronic core components
+    Layout.init(); // init current layout
+    Demo.init(); // init demo features
+});
+</script>
---
+++ resources/views/admin/head.blade.php
@@ -0,0 +1,13 @@
+<link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all" rel="stylesheet" type="text/css">
+<link href="{{ url("admin/assets/global/plugins/font-awesome/css/font-awesome.min.css") }}" rel="stylesheet" type="text/css">
+<link href="{{ url("admin/assets/global/plugins/simple-line-icons/simple-line-icons.min.css") }}" rel="stylesheet" type="text/css">
+<link href="{{ url("admin/assets/global/plugins/bootstrap/css/bootstrap.min.css") }}"  rel="stylesheet" type="text/css">
+<link href="{{ url("admin/assets/global/plugins/uniform/css/uniform.default.css") }}"  rel="stylesheet" type="text/css">
+<link href="{{ url("admin/assets/global/plugins/bootstrap-switch/css/bootstrap-switch.min.css") }}" rel="stylesheet" type="text/css"/>
+<!-- END GLOBAL MANDATORY STYLES -->
+<!-- BEGIN THEME STYLES -->
+<link href="{{ url("admin/assets/global/css/components-rounded.css") }}" id="style_components" rel="stylesheet" type="text/css"/>
+<link href="{{ url("admin/assets/global/css/plugins.css") }}" rel="stylesheet" type="text/css"/>
+<link href="{{ url("admin/assets/admin/layout4/css/layout.css") }}" rel="stylesheet" type="text/css"/>
+<link id="style_color" href="{{ url("admin/assets/admin/layout4/css/themes/light.css") }}" rel="stylesheet" type="text/css"/>
+<link href="{{ url("admin/assets/admin/layout4/css/custom.css") }}" rel="stylesheet" type="text/css"/>
