941e649654790abc684a9c5efbdb444fa897a084
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-10-29 21:27:39 +0200

    update home url in nav

--- resources/views/admin/nav.blade.php
+++ resources/views/admin/nav.blade.php
@@ -3,7 +3,7 @@
     <div class="page-header-inner">
         <!-- BEGIN LOGO -->
         <div class="page-logo">
-            <a href="index.html">
+            <a href="{{ route('/') }}">
                 <img src="{{ url("/images/logo.png") }}" alt="logo" class="logo-default"/>
             </a>
             <div class="menu-toggler sidebar-toggler">
